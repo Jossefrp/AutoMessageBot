8594430f706204917f08605eb9a3e6ba4258c112
Jossef <jossef.ramos.p@uni.pe>
2024-07-24 23:36:20 -0500

    Vista de la pantalla de carga de documento agregada

---
+++ views/ui_load.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'load_file.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QMainWindow,
+    QPushButton, QSizePolicy, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(960, 540)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.centralwidget.setEnabled(True)
+        self.centralwidget.setMinimumSize(QSize(960, 540))
+        self.centralwidget.setMaximumSize(QSize(960, 540))
+        self.centralwidget.setAutoFillBackground(False)
+        self.frame = QFrame(self.centralwidget)
+        self.frame.setObjectName(u"frame")
+        self.frame.setEnabled(True)
+        self.frame.setGeometry(QRect(0, 0, 960, 540))
+        self.frame.setMinimumSize(QSize(960, 540))
+        self.frame.setMaximumSize(QSize(960, 540))
+        self.frame.setStyleSheet(u"background-color: rgb(66, 66, 66);")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.label = QLabel(self.frame)
+        self.label.setObjectName(u"label")
+        self.label.setEnabled(True)
+        self.label.setGeometry(QRect(0, 147, 960, 27))
+        font = QFont()
+        font.setPointSize(15)
+        self.label.setFont(font)
+        self.label.setLayoutDirection(Qt.LeftToRight)
+        self.label.setStyleSheet(u"color: rgb(255, 255, 255);")
+        self.pushButton = QPushButton(self.frame)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setEnabled(True)
+        self.pushButton.setGeometry(QRect(365, 315, 230, 50))
+        self.pushButton.setFont(font)
+        self.pushButton.setStyleSheet(u"background-color: rgb(79, 117, 255);\n"
+"color: rgb(255, 255, 255);\n"
+"border-radius: 20px;")
+        self.label_2 = QLabel(self.frame)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(420, 195, 120, 75))
+        self.label_2.setPixmap(QPixmap(u"assets/icons/loadFile/excel.png"))
+        self.label_2.setScaledContents(True)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p align=\"center\">Sube un archivo de excel para extraer los datos</p></body></html>", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Subir archivo", None))
+        self.label_2.setText("")
+    # retranslateUi
+
