cba2dfa884cf19ef25115ff1f4efee5b0381df16
Jossef <jossef.ramos.p@uni.pe>
2024-07-31 20:40:08 -0500

    Agregado los logs para cada una de las pestañas

--- controllers/main.py
+++ controllers/main.py
@@ -10,27 +10,44 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
         self.ui.stackedWidget.setCurrentWidget(self.ui.select_column)
         self.ui.start_button_3.clicked.connect(
-            lambda: self.next_page(self.ui.main))
+            lambda: self.next_page(self.ui.main)
+        )
+        self.pages = {
+            "main": self.main_widget(),
+            "load_browser": self.load_browser_widget(),
+            "load_qr": self.load_qr_widget(),
+            "main_run": self.main_run(),
+            "finish": self.finish(),
+            "error": self.error()
+        }
+
         center_window(self)
+
+    def main_widget(self):
+        logger.info("main widget loading")
+        self.ui.start_main_button.clicked.connect(
+            lambda: self.next_page(self.ui.load_browser)
+        )
+
+    def load_browser_widget(self):
+        logger.info("main widget loading")
+
+    def load_qr_widget(self):
+        logger.info("load qr widget loading")
+        self.ui.start_qr_button.clicked.connect(
+            lambda: self.next_page(self.ui.load_browser)
+        )
+
+    def main_run(self):
+        logger.info("main run widget loading")
+
+    def finish(self):
+        logger.info("finish widget loading")
+
+    def error(self):
+        logger.info("error widget loading")
 
     def next_page(self, next_page: QWidget):
         logger.info(f"Abriendo la pestaña {next_page.objectName()}")
+        self.pages[next_page.objectName()]
         self.ui.stackedWidget.setCurrentWidget(next_page)
-
-    def main_widget(self):
-        pass
-
-    def load_browser_widget(self):
-        pass
-
-    def load_qr_widget(self):
-        pass
-
-    def main_run(self):
-        pass
-
-    def finish(self):
-        pass
-
-    def error(self):
-        pass
